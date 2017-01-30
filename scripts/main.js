546338d77adb74ae7b746889f44a83b6bf85838e
albinhag <albinhag@gmail.com>
2017-01-30 16:31:02 +0100

    drarochsover

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,77 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    resetSliders: true,
-    verticalCentered: false,
-    scrollBar: true,
+    // verticalCentered: false,
+    // scrollBar: true,
     anchors:['first', 'second', 'third'],
-    normalScrollElements: '.scrollable-element',
-  });
+    // normalScrollElements: '.scrollable-element',
+
+    //Navigation
+       menu: '.header',
+       lockAnchors: false,
+      //  anchors:['page1', 'page2', 'page3'],
+       navigation: false,
+       navigationPosition: 'right',
+       navigationTooltips: ['firstSlide', 'secondSlide'],
+       showActiveTooltip: false,
+       slidesNavigation: false,
+       slidesNavPosition: 'bottom',
+
+       //Scrolling
+       css3: true,
+       scrollingSpeed: 700,
+       autoScrolling: true,
+       fitToSection: true,
+       fitToSectionDelay: 1000,
+       scrollBar: true,
+       easing: 'easeInOutCubic',
+       easingcss3: 'ease',
+       loopBottom: false,
+       loopTop: false,
+       loopHorizontal: true,
+       continuousVertical: false,
+       continuousHorizontal: false,
+       scrollHorizontally: false,
+       interlockedSlides: false,
+       dragAndMove: false,
+       offsetSections: false,
+       resetSliders: true,
+       fadingEffect: false,
+       scrollOverflow: false,
+       scrollOverflowOptions: null,
+       touchSensitivity: 15,
+       normalScrollElementTouchThreshold: 5,
+       bigSectionsDestination: null,
+
+       //Accessibility
+       keyboardScrolling: true,
+       animateAnchor: false,
+       recordHistory: true,
+
+       //Design
+       controlArrows: true,
+       verticalCentered: false,
+
+       responsiveWidth: 0,
+       responsiveHeight: 0,
+       responsiveSlides: false,
+
+       //Custom selectors
+       sectionSelector: '.section',
+       slideSelector: '.slide',
+
+       lazyLoading: false,
+
+       //events
+       onLeave: function(index, nextIndex, direction){},
+       afterLoad: function(anchorLink, index){},
+       afterRender: function(){},
+       afterResize: function(){},
+       afterResponsive: function(isResponsive){},
+       afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+       onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+   });
+
+
 
   //CHECK PATH LENGTH
  //  var path = document.querySelector('.lines');
@@ -13,20 +79,20 @@ $(document).ready(function() {
  // console.log(length);
 
 //SMOOTH SCROLLING
-    $(function() {
-        $('a[href*="#"]:not([href="#"])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    });
+    // $(function() {
+    //     $('a[href*="#"]:not([href="#"])').click(function() {
+    //         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    //             var target = $(this.hash);
+    //             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //             if (target.length) {
+    //                 $('html, body').animate({
+    //                     scrollTop: target.offset().top
+    //                 }, 1000);
+    //                 return false;
+    //             }
+    //         }
+    //     });
+    // });
 //SCROLL TO ANIMATE
     var $animation_elements = $('#albColor');
     var $window = $(window);
